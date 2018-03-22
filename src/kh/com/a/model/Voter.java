30459c46f32c5862815478e45a50222f77ff8acb
Parker <qkrrudtjr954@kangwon.ac.kr>
2018-03-22 15:02:59 +0900

    vote setting

---
+++ src/kh/com/a/model/Voter.java
@@ -0,0 +1,75 @@
+package kh.com.a.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Voter implements Serializable {
+	private int voterid;
+	private int pollid;
+	private int pollsubid;	//	선택한 답 
+	private String id;
+	private Date regdate;
+	
+	public Voter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Voter(int voterid, int pollid, int pollsubid, String id, Date regdate) {
+		super();
+		this.voterid = voterid;
+		this.pollid = pollid;
+		this.pollsubid = pollsubid;
+		this.id = id;
+		this.regdate = regdate;
+	}
+
+	public int getVoterid() {
+		return voterid;
+	}
+
+	public void setVoterid(int voterid) {
+		this.voterid = voterid;
+	}
+
+	public int getPollid() {
+		return pollid;
+	}
+
+	public void setPollid(int pollid) {
+		this.pollid = pollid;
+	}
+
+	public int getPollsubid() {
+		return pollsubid;
+	}
+
+	public void setPollsubid(int pollsubid) {
+		this.pollsubid = pollsubid;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return "Voter [voterid=" + voterid + ", pollid=" + pollid + ", pollsubid=" + pollsubid + ", id=" + id
+				+ ", regdate=" + regdate + "]";
+	}
+	
+	
+	
+	
+}
